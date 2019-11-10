6dd8bce6037591e5d7c214648d67d406b211c29c
Jasper Cheung <wymanjasper@gmail.com>
2019-11-10 02:53:17 -0500

    companies

--- client/src/containers/CompanyView.js
+++ client/src/containers/CompanyView.js
@@ -1,13 +1,13 @@
 import CompanyCard from '../components/CompanyCard';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-
 import React from 'react';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import Radio from '@material-ui/core/Radio';
 import Paper from '@material-ui/core/Paper';
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,8 +24,6 @@ const useStyles = makeStyles(theme => ({
             backgroundColor: "#edf3f3",
         },
     },
-   
-
 }));
 
 export default function CompanyView() {
@@ -34,12 +32,11 @@ export default function CompanyView() {
     <Grid container className={classes.root} spacing={2}>
       <Grid item xs={12}>
         <Grid container justify="center" spacing={2}>
-          {[0, 1, 2,4].map(value => (
+          {[0, 1, 2,4,5,6,7,8].map(value => (
             <Grid key={value} item>
-             <CompanyCard/>
+            <CompanyCard data={{company:'BlockStack',role:'Software Engineering Intern', location: 'New York City, New York', image:"https://cdn.shopify.com/s/files/1/2396/7733/products/Logo_300x300.png?v=1560555268"}}/>
             </Grid>
           ))}
-
         </Grid>
       </Grid>
     </Grid>
