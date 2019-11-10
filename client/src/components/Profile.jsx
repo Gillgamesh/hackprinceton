05733565d5073d3c5362fcaa8b2376b2114a355e
srafi1 <shakilrafi0@gmail.com>
2019-11-09 19:24:00 -0500

    profile route

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { useBlockstack } from 'react-blockstack';
+ 
+export default function Profile () {
+    return (
+      <h1>Profile</h1>
+    );
+}
+
+
--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -2,15 +2,22 @@ import React, {useContext} from 'react';
 import { Home, Notifications, AccountCircle } from '@material-ui/icons';
 
 import LoginButton from './components/LoginButton';
+import Profile from './components/Profile';
 
 const Routes = [
-
     {
         path: '/',
         sidebarName: 'home',
         navbarName: 'home',
         icon: Home,
         component: LoginButton,
+    },
+    {
+        path: '/profile',
+        sidebarName: 'profile',
+        navbarName: 'profile',
+        icon: AccountCircle,
+        component: Profile,
     }
 ];
 
