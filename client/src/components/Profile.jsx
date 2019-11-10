4e5f26bab4cbef3116aa729bd2d57dcf1c679b66
srafi1 <shakilrafi0@gmail.com>
2019-11-09 19:55:29 -0500

    start profile page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
 import { useBlockstack } from 'react-blockstack';
- 
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    '@global': {
+        body: {
+            backgroundColor: theme.palette.common.white,
+        },
+    },
+    paper: {
+        marginTop: theme.spacing(12),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+}));
+
 export default function Profile () {
+    let classes = useStyles();
     return (
-      <h1>Profile</h1>
+        <div className={classes.paper}>
+            <h1>Profile</h1>
+        </div>
     );
 }
-
-
