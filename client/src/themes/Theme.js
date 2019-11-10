52482c07e0f0f3065ab0aaa925285b0428548d45
srafi1 <shakilrafi0@gmail.com>
2019-11-10 00:30:19 -0500

    black theme

--- client/src/themes/Theme.js
+++ client/src/themes/Theme.js
@@ -3,17 +3,17 @@ import React from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import Checkbox from '@material-ui/core/Checkbox';
-import red from '@material-ui/core/colors/red';
-import green from '@material-ui/core/colors/green';
+import deepPurple from '@material-ui/core/colors/deepPurple';
+import grey from '@material-ui/core/colors/grey';
 
 const outerTheme = createMuiTheme({
   palette: {
       primary : {
-          light: green['A100'],
-          main: green['A100'],
-          dark: green[100],
+          light: '#000000',
+          main: '#000000',
+          dark: '#000000',
       },
-      secondary:red,
+      secondary: deepPurple,
   },
 });
 
