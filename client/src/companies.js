153f2b3ff8ff44b340c30852d681615156218103
srafi1 <shakilrafi0@gmail.com>
2019-11-10 03:22:57 -0500

    company info

---
+++ client/src/companies.js
@@ -0,0 +1,50 @@
+export default let companies = [
+    {
+        company:'BlockStack',
+        role:'Software Engineering Intern',
+        location: 'New York City, NY',
+        image:"https://cdn.shopify.com/s/files/1/2396/7733/products/Logo_300x300.png?v=1560555268"
+    },
+    {
+        company:'Standard Library',
+        role:'Software Engineering Intern',
+        location: 'San Francisco, CA',
+        image:"https://content.public.files.stdlib.com/www/static/images/standard-library-logo-wordmark.svg"
+    },
+    {
+        company:'Bloomberg',
+        role:'Software Engineering Intern',
+        location: 'New York City, NY',
+        image:"https://pbs.twimg.com/profile_images/991818020233404416/alrBF_dr_400x400.jpg"
+    },
+    {
+        company:'Google',
+        role:'Software Engineering Intern',
+        location: 'Mountain View, CA',
+        image:"https://blocktribune.com/wp-content/uploads/2018/09/google.jpg"
+    },
+    {
+        company:'Facebook',
+        role:'Software Engineering Intern',
+        location: 'New York City, NY',
+        image:"https://www.facebook.com/images/fb_icon_325x325.png"
+    },
+    {
+        company:'IBM',
+        role:'Software Engineering Intern',
+        location: 'Poughkeepsie, NY',
+        image:"https://upload.wikimedia.org/wikipedia/commons/5/51/IBM_logo.svg"
+    },
+    {
+        company:'Intel',
+        role:'Software Engineering Intern',
+        location: 'Santa Clara, NY',
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Intel-logo.svg/1200px-Intel-logo.svg.png"
+    },
+    {
+        company:'Amazon',
+        role:'Software Engineering Intern',
+        location: 'Seattle, WA',
+        image:"http://g-ec2.images-amazon.com/images/G/01/social/api-share/amazon_logo_500500._V323939215_.png"
+    },
+];
