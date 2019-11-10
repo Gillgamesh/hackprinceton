473f147893727f282cc4c35b8be212b2550c8d54
srafi1 <shakilrafi0@gmail.com>
2019-11-09 23:20:55 -0500

    profile form page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import { useBlockstack } from 'react-blockstack';
+import {useBlockstack, useFile} from 'react-blockstack';
+import {getFile, putFile} from 'blockstack';
 import {makeStyles} from '@material-ui/core/styles';
+import {Button} from '@material-ui/core';
+
+import {Formik, Field, Form} from 'formik';
+import {TextField} from 'formik-material-ui';
+import * as Yup from 'yup';
 
 const useStyles = makeStyles(theme => ({
     '@global': {
@@ -15,13 +21,79 @@ const useStyles = makeStyles(theme => ({
         flexDirection: 'column',
         alignItems: 'center',
     },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    submit: {
+        marginBottom: theme.spacing(1)
+    },
+    field: {
+        width: '50%',
+        margin: theme.spacing(1),
+    },
 }));
 
+const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required("Required"),
+    lastName: Yup.string().required("Required"),
+})
+
 export default function Profile () {
-    let classes = useStyles();
+    getFile("user.json")
+        .then((file) => {
+            let obj = JSON.parse(file);
+            if (!file) return;
+            setState({firstName: obj.firstName, lastName: obj.lastName});
+        });
+    const [state, setState] = useState({firstName:"", lastName:""});
+    const classes = useStyles();
     return (
         <div className={classes.paper}>
             <h1>Profile</h1>
+                <Formik
+                    enableReinitialize={true}
+                    validationSchema={validationSchema}
+                    initialValues={{
+                    firstName: state.firstName,
+                    lastName: state.lastName,
+                    }}
+                    onSubmit = {
+                    (values, {setSubmitting}) => {
+                    putFile("user.json", JSON.stringify(values))
+                    }
+                    }
+                    render = {
+                    ({submitForm, isSubmitting, values, setFieldValue}) => (
+                    <Form className={classes.form}>
+                        <Field
+                            className={classes.field}
+                            name="firstName"
+                            label="First Name"
+                            component={TextField} />
+                        <Field
+                            className={classes.field}
+                            name="lastName"
+                            label="Last Name"
+                            component={TextField} />
+                        <Button
+                            className={classes.submit}
+                            disabled={isSubmitting}
+                            onClick={submitForm}>
+                            Submit
+                        </Button>
+                    </Form>
+                    )
+                    }
+                />
+
         </div>
     );
 }
